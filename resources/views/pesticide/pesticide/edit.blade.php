34baf7358e888dd6e83c288a41ae84fdac758854
fazesantaluiza <fazesantaluiza@gmail.com>
2021-07-13 15:52:02 -0300

    subindo ajustes nos avatar

--- resources/views/pesticide/pesticide/edit.blade.php
+++ resources/views/pesticide/pesticide/edit.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
   
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Defencivo</title>
+    <title>Defensivo</title>
      <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
