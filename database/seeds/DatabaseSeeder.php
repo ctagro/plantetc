d4be22db54377a0bba789c8daa750842209a5fef
fazesantaluiza <fazesantaluiza@gmail.com>
2022-08-05 18:31:30 -0300

    Acerto do fluxo de caixa para uso do wemerson 2

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+     ///  $this->call(UserTableSeeder::class);
+     /// $this->call(GroundTableSeeder::class);
+    /// $this->call(ActivityTableSeeder::class);
+     //  $this->call(AccountTableSeeder::class);
+     /// $this->call(WorkerTableSeeder::class);
+     /// $this->call(Type_activityTableSeeder::class);
+    /// $this->call(AccountingTableSeeder::class);
+      ///  $this->call(CropTableSeeder::class);
+     ///  $this->call(ProductTableSeeder::class);
+      ///  $this->call(BayerTableSeeder::class);
+     ///   $this->call(TypeAccountSeeder::class);
+      // $this->call(ProductApplySeeder::class); 
+      // $this->call(SaleTableSeeder::class);
+      //  $this->call(CompetenceSeeder::class);
+        $this->call(StatusInventorySeeder::class);
+
+
+    }
+}
